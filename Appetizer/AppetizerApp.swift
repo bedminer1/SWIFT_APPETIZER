5fff312b6a3ee7e04919233b9c4d7b842bb5f79b
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-08 01:31:18 +0800

    init

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Alex on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
