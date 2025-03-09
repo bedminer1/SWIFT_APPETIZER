818c22f21ba07e5c8f98d6fab5a794f2d562f160
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-09 17:31:27 +0800

    added loading spinner

---
+++ Appetizer/Utilities/Extensions/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizer
+//
+//  Created by Alex on 9/3/25.
+//
+
+import SwiftUI
+
+//extension Color {
+//    static let brandPrimary = Color("brandPrimary")
+//}
+//
+//extension UIColor {
+//    static let brandPrimary = UIColor(named: "brandPrimary")
+//}
--- Appetizer/Views/AppetizerListView.swift
+++ Appetizer/Views/AppetizerListView.swift
@@ -11,14 +11,21 @@ struct AppetizerListView: View {
     @StateObject var viewModel = AppetizerListViewModel()
     
     var body: some View {
-        NavigationView {
-            List(viewModel.appetizers) { a ppetizer in
-                AppetizerListCellView(appetizer: appetizer)
+        ZStack {
+            
+            NavigationView {
+                List(viewModel.appetizers) { appetizer in
+                    AppetizerListCellView(appetizer: appetizer)
+                }
+                .navigationTitle(Text("Appetizers"))
             }
-            .navigationTitle(Text("Appetizers"))
-        }
-        .onAppear {
-            viewModel.getAppetizers()
+            .onAppear {
+                viewModel.getAppetizers()
+            }
+            
+            if viewModel.isLoading {
+                LoadingView()
+            }
         }
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title,
